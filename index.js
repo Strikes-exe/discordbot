fbc5e0cf8176cd42d4ae1a721f6e1ecaae6fa8ac
Abdurahman A <strikesyessir@gmail.com>
2021-02-05 00:17:32 -0800

    my first commit

---
+++ index.js
@@ -0,0 +1,148 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+
+bot.on('message', (message) => {
+
+    switch (message.content.toLowerCase()) {
+
+        case '*bs': // Bs = Bot Speed
+            message.reply('Ok');
+            break;
+
+        case '*date':
+            var date = new Date();
+            message.reply(`The date is ${date}`);
+            break;
+
+        case '*si': // Server Info
+            message.reply('Contact the owner of the server for server info!');
+            break;
+
+        case '*bc': // Bot Creator
+            message.reply("This bot was created by Strikes#6969");
+            break;
+
+
+        case "*invite": // Bot Invite
+            message.reply("https://discord.com/api/oauth2/authorize?client_id=798682069008908378&permissions=8&scope=bot")
+             break;
+    
+        // case "Cmds": // BOT CMDS
+        //     message.reply()
+    
+    
+        case "*cmds": 
+            message.reply("*bs: Bot Speed  |  *si:  Server Info  |  *date: Time&Date (Western Timezone)  |  *bc: Bot Creator  | *invite: Bot Invite (Invite My Bot In Your Server)")
+       break
+    
+}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bot.on('ready', () => {
+    console.log('This bot is online!')
+    bot.user.setActivity("*cmds")
+})
+
+
+
+
+
+bot.login('Nzk4NjgyMDY5MDA4OTA4Mzc4.X_4kpA.Tz6mIm_PEj8Cduc2laQWEWCjU2A');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // if (message.content.toLowerCase() == 'ping') {
+    //     message.reply('Pong!');
+
+    // }
+
+    // if (message.content.toLowerCase() == 'hi' || message.content.toLowerCase() == "hello") {
+    //     message.reply('Hi');
+
+    // }
+
+
+    // if (message.content.toLowerCase() == 'date') {
+    //     var date = new Date();
+    //     message.reply(`The date is ${date}`);
+
+    // }
+
